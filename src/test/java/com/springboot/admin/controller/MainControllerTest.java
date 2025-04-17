509aeeebe22cef5a67c781c47d59fb714fb44a77
MJ0501 <mjay.do@gmail.com>
2025-04-17 23:01:53 +0900

    View Endpoint(Controller) Test 작성
    Test는 뼈대만. MainController/AdminAccountController/ArticleManagementController/ArticleCommentManagementController/UserAccountManagementController

---
+++ src/test/java/com/springboot/admin/controller/MainControllerTest.java
@@ -0,0 +1,35 @@
+package com.springboot.admin.controller;
+
+import com.springboot.admin.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("Root Controller")
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+    private final MockMvc mvc;
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @WithMockUser(username = "tester", roles = "USER")
+    @DisplayName("[GET] Root Page -> Forwarding Article Management Page")
+    @Test
+    void whenRequestingRootView_thenForwardsToArticleManagementView() throws Exception {
+        // When & Then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/management/articles"))
+                .andExpect(forwardedUrl("/management/articles"));
+    }
+}
---
+++ src/test/java/com/springboot/admin/controller/UserAccountManagementControllerTest.java
@@ -0,0 +1,35 @@
+package com.springboot.admin.controller;
+
+import com.springboot.admin.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("Controller - User Account Management")
+@Import(SecurityConfig.class)
+@WebMvcTest(UserAccountManagementController.class)
+class UserAccountManagementControllerTest {
+    private final MockMvc mvc;
+    public UserAccountManagementControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+    @WithMockUser(username = "tester", roles = "USER")
+    @DisplayName("[GET] User Account Management Page - 정상 호출")
+    @Test
+    void whenRequestingUserAccountManagementView_thenReturnsUserAccountManagementView() throws Exception {
+        // When & Then
+        mvc.perform(get("/management/user-accounts"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("management/user-accounts"))
+                .andExpect(model().attribute("userAccounts", List.of()));
+    }
+}
